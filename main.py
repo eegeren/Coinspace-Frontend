5fe642b273a6ac5a964c771ad6c066a838477efc
eegeren <yusufege.erenn@gmail.com>
2025-06-23 15:13:40 +0300

    Fix

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
 from fastapi import FastAPI
-from fastapi.responses import FileResponse
-from fastapi.staticfiles import StaticFiles
-import os
+from fastapi.middleware.cors import CORSMiddleware
+from api.signal_api import router as signal_router
+from api.calendar_api import router as calendar_router
+from api.coin_api import router as coin_router
+from api.news_api import router as news_router
 
 app = FastAPI()
 
-# Statik klasörü mount et
-app.mount("/static", StaticFiles(directory="static"), name="static")
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-# Ana sayfa için index.html'i döndür
-@app.get("/")
-async def root():
-    return FileResponse("static/index.html")
-
-# Diğer API router'larını burada import et
-from api.coin_api import router as coin_router
-#from api.signal_api import router as signal_router
-#from api.calendar_api import router as calendar_router
-
-#app.include_router(coin_router, prefix="/api/coin")
-#app.include_router(signal_router, prefix="/api/signal")
-#app.include_router(calendar_router, prefix="/api/calendar")
+app.include_router(signal_router, prefix="/api/signal")
+app.include_router(calendar_router, prefix="/api/calendar")
+app.include_router(coin_router, prefix="/api/coins")
+app.include_router(news_router, prefix="/api/news")
