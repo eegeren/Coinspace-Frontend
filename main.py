1e21a838fa515fc22255e69f8480f69ce902029d
eegeren <yusufege.erenn@gmail.com>
2025-06-23 15:49:08 +0300

    Fix

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+
+# Router importları (bunlar dosyalarında `router = APIRouter()` olarak tanımlanmış olmalı)
 from api.signal_api import router as signal_router
-from api.news_api import router as news_router
+from api.coin_api import router as coin_router
 from api.calendar_api import router as calendar_router
+from api.news_api import router as news_router
 
 app = FastAPI()
 
+# CORS ayarı
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -16,12 +19,16 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.mount("/", StaticFiles(directory="static", html=True), name="static")
-
+# API routerlarını bağlama
 app.include_router(signal_router, prefix="/api")
-app.include_router(news_router, prefix="/api")
+app.include_router(coin_router, prefix="/api")
 app.include_router(calendar_router, prefix="/api")
+app.include_router(news_router, prefix="/api")
 
-@app.get("/")
-def read_index():
-    return FileResponse("static/index.html")
+# Ana sayfa kontrolü
+@app.get("/api")
+def read_root():
+    return {"message": "Coinspace API is working!"}
+
+# Statik dosyaları servis et (HTML, JS, CSS)
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
